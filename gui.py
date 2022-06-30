bb6a91e47a85b515dd65e27c7417a1645dbe3ce8
Sümeyra AKSEKİ BOZ <sumeyraakseki86@gmail.com>
2022-06-30 14:09:03 +0300

    Update gui.py

--- gui.py
+++ gui.py
@@ -37,7 +37,7 @@ while True:
         break
     elif k%256 == 32:
         # SPACE pressed
-        img_name = "img{}.jpg".format(img_counter)
+        img_name = "im{}.jpg".format(img_counter)
         cv2.imwrite(img_name, frame)
         print("{} resim kaydediliyor!".format(img_name))
         img_counter += 1
